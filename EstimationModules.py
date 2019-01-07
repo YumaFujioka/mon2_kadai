fa94d4164cab60f7db9a4004083fc11e41b936e6
YumaFujioka <tpddfs@live.jp>
2019-01-08 02:44:30 +0900

    zf, mmseの実装

--- EstimationModules.py
+++ EstimationModules.py
@@ -1,5 +1,22 @@
 import numpy as np
 
 class Estimator():
-    def __init__(self):
+    def __init__(self, M, N):
+        self.M = M
+        self.N = N
+
+    def zf(self, H, rx_syms, N_0):
+        if self.M == self.N:
+            Weight = np.linalg.inv(H)
+        else:
+            Weight = np.linalg.pinv(H)
+        tx_syms_hat = Weight @ rx_syms
+        return tx_syms_hat
+
+    def mmse(self, H, rx_syms, N_0):
+        Weight = np.linalg.inv(H @ np.conjugate(H.T) + N_0 * np.eye(self.N)) @ H
+        tx_syms_hat = np.conjugate(Weight.T) @ rx_syms
+        return tx_syms_hat
+
+    def mld(self, H, rx_syms, N_0):
         pass
--- MimoSimulator.py
+++ MimoSimulator.py
@@ -1,7 +1,3 @@
-# estimator = EstimationModules.Estimator()
-
-
-
 import numpy as np
 import EstimationModules
 import argparse
@@ -20,13 +16,28 @@ parser.add_argument("-i", "--IterNum", action="store", default=50000)
 parser.add_argument("-b", "--BitsNum", action="store", default=256)
 args = parser.parse_args()
 
+def bits2QPSK(bits):
+    BPSK_T = 2.0 * bits.T - 1.0
+    QPSK_T = (BPSK_T[0::2] + 1j * BPSK_T[1::2]) / np.sqrt(2)
+    QPSK = QPSK_T.T # ただの転置。書きやすくするために転置してるだけ。
+    return QPSK
+
+def QPSK2bits(QPSK):
+    re_bits = np.where(QPSK.real > 0, 1, 0)
+    im_bits = np.where(QPSK.imag > 0, 1, 0)
+    bits = np.insert(re_bits, np.arange(1, len(re_bits[0])+1), im_bits, axis=1)
+    return bits
+
 if __name__ == "__main__":
+
     # 入力アンテナ数、出力アンテナ数
-    M = args.SigNum
-    N = args.ObsNum
+    M = int(args.SigNum)
+    N = int(args.ObsNum)
+    # 推測器
+    estimator = EstimationModules.Estimator(M, N)
     # ループ数、シンボルビット数
-    IterNum = args.IterNum
-    BitsNum = args.BitsNum
+    IterNum = int(args.IterNum)
+    BitsNum = int(args.BitsNum)
 
     # 0〜40dBでシミュレート
     for SNR_dB in np.linspace(0, 40.0, 11):
@@ -39,4 +50,17 @@ if __name__ == "__main__":
 
         #####ctはappendで処理？
 
-        for 
+        for i in range(IterNum):
+            data_bits = np.where(np.random.rand(M, BitsNum) > 0.5, 1, 0)                                          # M個の(BitsNum)bit列を作成
+            tx_syms = bits2QPSK(data_bits)                                                                        # シンボル系列。QPSKなので(BitsNum)/2個のシンボル系列が出来る
+            H_mat = (np.random.randn(N, M) + 1j * np.random.randn(N, M)) / np.sqrt(2)                             # 通信路行列
+            noise_syms = (np.random.randn(N, int(BitsNum/2)) + 1j * np.random.randn(N, int(BitsNum/2))) * sigma   # 雑音シンボル
+
+            rx_syms = H_mat @ tx_syms + noise_syms                                  # 通信路通過
+            tx_syms_hat = eval("estimator." + args.Method)(H_mat, rx_syms, N_0)     # 推定
+            est_bits = QPSK2bits(tx_syms_hat)                                       # 推定シンボル(QPSK)をビットに変換
+
+            num_of_error_bit += np.sum( np.sum( np.abs(data_bits - est_bits)))      # 誤りビット数をカウント
+
+        error_rate = num_of_error_bit / (IterNum * BitsNum * M)
+        print(error_rate)
